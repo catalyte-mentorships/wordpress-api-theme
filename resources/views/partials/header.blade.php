65cc409746b79e48acccd874eb65aca75371bde4
nomad-mystic <nomadmystics@gmail.com>
2019-03-15 20:53:36 -0700

    build_nav: Working throught the header and nav for the site. Next up is the off-canvas menu comonent.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,31 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+    <section class="nav">
+      <div class="nav__logo">
+        <a href="/" class="nav__logo-svg"></a>
+      </div>
+      <div class="nav__controls">
+        <a href="https://rsecure.metmuseum.org/admissions/tickets" class="buy-tickets">Buy Tickets</a>
+        <div class="search">
+          <a href="#">
+            <span class="icon"></span>
+          </a>
+        </div>
+
+        <a href="#main-menu" class="menu-toggle">
+          <span class="fa fa-bars"></span>
+        </a>
+
+        <nav class="nav__menu">
+          <a href="#main-menu-toggle" class="menu-close">
+            <span class="fa fa-close"></span>
+          </a>
+          {{--@if (has_nav_menu('primary_navigation'))--}}
+            {{--{!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}--}}
+          {{--@endif--}}
+        </nav>
+        <a href="#main-menu-toggle" class="backdrop" hidden></a>
+      </div><!--controls-->
+    </section>
   </div>
 </header>
