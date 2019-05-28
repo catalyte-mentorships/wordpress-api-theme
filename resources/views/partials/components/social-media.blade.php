86aa5538f3587686310e3b06a1ab438f8626b403
nomad-mystic <nomadmystics@gmail.com>
2019-05-27 21:34:40 -0700

    build_footer: Worked out the social media component for the footer and signup for the newsletter.

---
+++ resources/views/partials/components/social-media.blade.php
@@ -0,0 +1,29 @@
+<section>
+  <div class="footer__social-media">
+    <span>
+      <a class="media-link" href="https://www.facebook.com/metmuseum" target="_blank">
+        <i class="fab fa-facebook"></i>
+      </a>
+    </span>
+    <span>
+      <a class="media-link" href="https://twitter.com/metmuseum" target="_blank">
+        <i class="fab fa-twitter"></i>
+      </a>
+    </span>
+    <span>
+      <a class="media-link" href="https://www.pinterest.com/metmuseum/?autologin=true" target="_blank">
+        <i class="fab fa-pinterest"></i>
+      </a>
+    </span>
+    <span>
+      <a class="media-link" href="https://www.instagram.com/metmuseum/" target="_blank">
+        <i class="fab fa-instagram"></i>
+      </a>
+    </span>
+    <span>
+      <a class="media-link" href="https://www.facebook.com/metmuseum" target="_blank">
+        <i class="fab fa-youtube"></i>
+      </a>
+    </span>
+  </div>
+</section>
