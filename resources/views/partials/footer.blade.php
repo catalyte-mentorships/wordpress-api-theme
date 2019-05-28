437279ee0e57374e9742ed943473ded769105695
nomad-mystic <nomadmystics@gmail.com>
2019-05-27 18:39:25 -0700

    master: Just some HTML. Got caried away before moving onto anther branch :smile:.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,18 +1,36 @@
 <footer class="content-info footer">
   <div class="container">
-    <div class="footer__newsletter">
-      <h3>Bring some culture to your inbox.</h3>
-      <div class="footer__newsletter-signup">
-        <input
-          id="newsletter-input"
-          class="newsletter-input"
-          type="email"
-          placeholder="Enter your email to sign up for our newsletter."
-        >
-        <button class="newsletter-submit">
-          <i class="fas fa-chevron-right"></i>
-        </button>
+    <section>
+      <div class="footer__newsletter">
+        <h3>Bring some culture to your inbox.</h3>
+        <div class="footer__newsletter-signup">
+          <input
+            id="newsletter-input"
+            class="newsletter-input"
+            type="email"
+            placeholder="Enter your email to sign up for our newsletter."
+          >
+          <button class="newsletter-submit">
+            <i class="fas fa-chevron-right"></i>
+          </button>
+        </div>
       </div>
-    </div>
+    </section>
+    <section>
+      <div class="footer__nav" role="navigation">
+        <h3>
+          <a href="" target="_blank">About the Met</a>
+        </h3>
+        <h3>
+          <a href="" target="_blank">Blogs</a>
+        </h3>
+        <h3>
+          <a href="" target="_blank">Videos</a>
+        </h3>
+        <h3>
+          <a href="" target="_blank">About the Met</a>
+        </h3>
+      </div>
+    </section>
   </div>
 </footer>
